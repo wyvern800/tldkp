bbd9106d341bf3eb66d1d9bc13c1057c6d34c4b8
Matheus Ferreira <matheusguiferreira@hotmail.com>
2024-10-14 01:34:28 -0300

    fix: api url

--- frontend/src/services/axiosInstance.tsx
+++ frontend/src/services/axiosInstance.tsx
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 // Create the axios instance
 const baseServer: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
