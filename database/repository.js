440e70e477b8ce30c086d56b5b446b69f9aa17be
Matheus Ferreira <matheusguiferreira@hotmail.com>
2024-10-20 19:39:59 -0300

    chore: added the functionality of disabling dms

--- database/repository.js
+++ database/repository.js
@@ -184,7 +184,12 @@ export async function guildCreate(guild) {
     createdAt: admin.firestore.FieldValue.serverTimestamp(),
     updatedAt: admin.firestore.FieldValue.serverTimestamp(),
     language: LANGUAGE_EN,
-    memberDkps: [],
+    memberDkps: [],    
+    togglables: {
+      dkpSystem: {
+        dmNotifications: true
+      }
+    }
   };
 
   const res = await db.collection("guilds").doc(guild.id).set(defaultConfig);
@@ -268,7 +273,7 @@ export async function handleUpdateDkp(interaction) {
         return interaction.reply({ content: errorMsg, ephemeral: true });
       }
 
-      setDkp(newDkp, user.id, amount, user, guildDataResponse?.guildData?.name);
+      setDkp(newDkp, user.id, amount, user, guildDataResponse?.guildData?.name, guildDataResponse);
 
       // Update the guild data with the new memberDkps array and the current timestamp
       newGuildData = {
@@ -294,7 +299,8 @@ export async function handleUpdateDkp(interaction) {
         user.id,
         choices === "add" ? amount : -amount,
         user,
-        guildDataResponse?.guildData?.name
+        guildDataResponse?.guildData?.name,
+        guildDataResponse
       );
 
       // Update the guild data with the new memberDkps array and the current timestamp
