2cec28850ce11f96fa0f84f140f119405099d34e
Matheus Ferreira <matheusguiferreira@hotmail.com>
2024-11-15 02:13:26 -0300

    fix: typo

--- utils/commands.js
+++ utils/commands.js
@@ -19,14 +19,7 @@ const PREFIX = "Discord.js/SlashCommands";
  * @returns { boolean } Wheter if its allowed or not
  */
 export function isInteractionPermitted(interaction, permissions) {
-
-  return permissions?.some((permission) => {
-    console.log(permission)
-    console.log(interaction?.member?.permissions)
-    console.log(interaction?.member?.permissions.has(permission))
-    return interaction?.member?.permissions.has(permission)
-    }
-  );
+  return permissions?.some((permission) => interaction?.member?.permissions.has(permission));
 }
 
 /**
