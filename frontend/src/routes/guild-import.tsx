f11e78a8f8c908f4f5d5cfe5e701065247e95a7e
Matheus Ferreira <matheusguiferreira@hotmail.com>
2025-09-10 23:50:41 -0300

    fix: any import

--- frontend/src/routes/guild-import.tsx
+++ frontend/src/routes/guild-import.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import {
   Box,
